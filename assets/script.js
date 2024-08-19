b71654869dd28066dc808513c494e85c83a455d2
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-08-19 22:42:33 +0000

    Initialised game variables and selected HTML elements

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const game = { cur: "", solution: "", puzz: [], total: 0, wrongGuesses: 0 };
+    const myWords = ["arrow functions", "arithmetic operators", "global variables", "else if statements", "primitive data types", "template literals"];
+    const score = document.querySelector(".score");
+    const puzzle = document.querySelector(".puzzle");
+    const letters = document.querySelector(".letters");
+    const btn = document.querySelector("button");
+    const hangmanParts = document.querySelectorAll('.hangman-part');  
+});
